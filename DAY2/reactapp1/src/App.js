71a7059c54a3ffd76086b736b98603094804cf01
Ashmitha C <124855048+Ashmitha14@users.noreply.github.com>
2023-11-16 12:30:15 +0530

    DAY2

---
+++ DAY2/reactapp1/src/App.js
@@ -0,0 +1,10 @@
+import './App.css';
+import Stylings from './component/Stylings';
+function App() {
+  return (
+    <div className="App">
+      <Stylings/>
+    </div>
+  );
+}
+export default App;
---
+++ DAY2/reactapp2/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import Stylings from './component/Stylings';
+function App() {
+  return (
+    <div className="App">
+      <Stylings/>
+    </div>
+  );
+}
+
+export default App;
