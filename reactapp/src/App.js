b0e0dbef11aca720cd26e5d1470722c9738691dd
Ashmitha C <124855048+Ashmitha14@users.noreply.github.com>
2023-11-16 12:34:46 +0530

    DAY3

---
+++ reactapp/src/App.js
@@ -0,0 +1,12 @@
+import ToogleMessage from './component/TogggleMessage';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <ToogleMessage/>
+    </div>
+  );
+}
+
+export default App;
